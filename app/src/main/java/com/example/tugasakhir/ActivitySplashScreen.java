3b9386f999af68ea7ab69d6b8890cadebcba8ebc
Jabal Logian <jaballogianrma77@gmail.com>
2019-06-01 12:52:59 +0700

    activity login, register, register device, and splash screen is initialized

---
+++ app/src/main/java/com/example/tugasakhir/ActivitySplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.tugasakhir;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import me.anwarshahriar.calligrapher.Calligrapher;
+
+public class ActivitySplashScreen extends AppCompatActivity {
+
+    private static int SPLASH_TIMED_OUT = 3000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_splash_screen);
+
+        Calligrapher calligrapher = new Calligrapher(this);
+        calligrapher.setFont(this, "PRODUCT_SANS.ttf", true);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run(){
+
+                Intent toMapsActivity = new Intent(ActivitySplashScreen.this, ActivityMain.class);
+                toMapsActivity.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(toMapsActivity);
+                finish();
+            }
+        },SPLASH_TIMED_OUT);
+    }
+}
